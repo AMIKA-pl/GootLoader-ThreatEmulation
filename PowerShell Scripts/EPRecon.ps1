b8dab38cd390835f1f2beb6af10a0c62e576ba0c
AMIKA <60911813+AMIKA-pl@users.noreply.github.com>
2022-10-06 12:19:12 +0800

    Delete EPRecon.ps1

--- PowerShell Scripts/EPRecon.ps1
+++
@@ -1,4 +0,0 @@
-$url = ''
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $url -OutFile $env:C:\Documents\EPRecon.ps1 
-Powershell.exe .\EPRecon.ps1
